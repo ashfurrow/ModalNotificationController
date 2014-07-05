86827762fc6165ff1190703cee731a6f3701f43b
Ash Furrow <ash@ashfurrow.com>
2014-07-05 19:07:16 +0200

    Basics of gesture recognizer down.

--- ModalNotificationViewController.swift
+++ ModalNotificationViewController.swift
@@ -19,6 +19,13 @@ class ModalNotificationViewController: UIViewController {
         let animator = UIDynamicAnimator(referenceView: self.view)
         return animator
     }()
+    @lazy var panRecognizer: UIPanGestureRecognizer = {
+        let panRecognizer = UIPanGestureRecognizer(target: self, action: "userDidPan:")
+        return panRecognizer
+    }()
+    var snapBehaviour: UISnapBehavior?
+    var pushBehaviour: UIPushBehavior?
+    var attachmentBehaviour: UIAttachmentBehavior?
     var currentViewController: UIViewController?
     var index: UInt {
         didSet {
@@ -45,6 +52,7 @@ class ModalNotificationViewController: UIViewController {
             currentViewController.removeFromParentViewController()
             currentViewController.view.removeFromSuperview()
             currentViewController.didMoveToParentViewController(nil)
+            removeBehavioursForCurrentController()
         }
         
         currentViewController = delegate.viewControllerAtIndex(index)
@@ -55,14 +63,70 @@ class ModalNotificationViewController: UIViewController {
             addChildViewController(currentViewController)
             view.addSubview(currentViewController.view)
             currentViewController.didMoveToParentViewController(self)
+            addBehavioursToCurrentController()
         }
         
         index++
     }
     
+    func removeBehavioursForCurrentController() {
+        animator.removeBehavior(snapBehaviour)
+        snapBehaviour = nil
+        animator.removeBehavior(pushBehaviour)
+        pushBehaviour = nil
+    }
+    
+    func addBehavioursToCurrentController() {
+        if let currentViewController = currentViewController {
+            snapBehaviour = UISnapBehavior(item: currentViewController.view, snapToPoint: self.view.center)
+            animator.addBehavior(snapBehaviour)
+            
+            pushBehaviour = UIPushBehavior(items: [currentViewController.view], mode: .Instantaneous)
+            pushBehaviour!.magnitude = 0
+            pushBehaviour!.angle = 0
+            animator.addBehavior(pushBehaviour)
+            
+            currentViewController.view.addGestureRecognizer(panRecognizer)
+        }
+    }
+    
     func checkForLastViewController() {
         if index == delegate.numberOfViewControllers() {
             self.presentingViewController?.dismissModalViewControllerAnimated(true)
+        }
+    }
+    
+    func userDidPan(panRecognizer: UIPanGestureRecognizer) {
+        // Many thanks to https://github.com/u10int/URBMediaFocusViewController
+        let location = panRecognizer.locationInView(self.view)
+        let boxLocation = panRecognizer.locationInView(self.currentViewController!.view)
+        
+        if (panRecognizer.state == .Began) {
+            animator.removeBehavior(snapBehaviour)
+            animator.removeBehavior(pushBehaviour)
+            let centerOffset = UIOffsetMake(boxLocation.x - CGRectGetMidX(self.currentViewController!.view.bounds), boxLocation.y - CGRectGetMidY(self.currentViewController!.view.bounds));
+            attachmentBehaviour = UIAttachmentBehavior(item: self.currentViewController!.view, offsetFromCenter: centerOffset, attachedToAnchor: location)
+            animator.addBehavior(attachmentBehaviour)
+        } else if (panRecognizer.state == .Changed) {
+            attachmentBehaviour!.anchorPoint = location
+        } else if (panRecognizer.state == .Ended) {
+            animator.removeBehavior(attachmentBehaviour)
+            attachmentBehaviour = nil
+            
+            let runningOnPad = UIDevice.currentDevice().userInterfaceIdiom == .Pad
+            // need to scale velocity values to tame down physics on the iPad
+            let deviceVelocityScale = runningOnPad ? 0.2 : 1.0
+            let deviceAngularScale = runningOnPad ? 0.7 : 1.0
+            // factor to increase delay before `dismissAfterPush` is called on iPad to account for more area to cover to disappear
+            let deviceDismissDelay = runningOnPad ? 1.8 : 1.0
+            let velocity = panRecognizer.velocityInView(view)
+            let velocityAdjust = 10.0 * deviceVelocityScale
+            
+            
+            let minimumVelocityRequiredForPush = 50.0
+            
+            animator.addBehavior(snapBehaviour)
+            animator.addBehavior(pushBehaviour)
         }
     }
 }
