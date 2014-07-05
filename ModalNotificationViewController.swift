1fe66408a8647f0dc9e7b45f2794f51ea4add8d7
Ash Furrow <ash@ashfurrow.com>
2014-07-05 19:30:56 +0200

    Basics of gesture recognizer done.

--- ModalNotificationViewController.swift
+++ ModalNotificationViewController.swift
@@ -26,6 +26,7 @@ class ModalNotificationViewController: UIViewController {
     var snapBehaviour: UISnapBehavior?
     var pushBehaviour: UIPushBehavior?
     var attachmentBehaviour: UIAttachmentBehavior?
+    var itemBehaviour: UIDynamicItemBehavior?
     var currentViewController: UIViewController?
     var index: UInt {
         didSet {
@@ -86,6 +87,9 @@ class ModalNotificationViewController: UIViewController {
             pushBehaviour!.angle = 0
             animator.addBehavior(pushBehaviour)
             
+            itemBehaviour = UIDynamicItemBehavior(items: [currentViewController.view])
+            animator.addBehavior(itemBehaviour)
+            
             currentViewController.view.addGestureRecognizer(panRecognizer)
         }
     }
@@ -98,14 +102,14 @@ class ModalNotificationViewController: UIViewController {
     
     func userDidPan(panRecognizer: UIPanGestureRecognizer) {
         // Many thanks to https://github.com/u10int/URBMediaFocusViewController
-        let location = panRecognizer.locationInView(self.view)
-        let boxLocation = panRecognizer.locationInView(self.currentViewController!.view)
+        let location = panRecognizer.locationInView(view)
+        let boxLocation = panRecognizer.locationInView(currentViewController!.view)
         
         if (panRecognizer.state == .Began) {
             animator.removeBehavior(snapBehaviour)
             animator.removeBehavior(pushBehaviour)
-            let centerOffset = UIOffsetMake(boxLocation.x - CGRectGetMidX(self.currentViewController!.view.bounds), boxLocation.y - CGRectGetMidY(self.currentViewController!.view.bounds));
-            attachmentBehaviour = UIAttachmentBehavior(item: self.currentViewController!.view, offsetFromCenter: centerOffset, attachedToAnchor: location)
+            let centerOffset = UIOffsetMake(boxLocation.x - CGRectGetMidX(currentViewController!.view.bounds), boxLocation.y - CGRectGetMidY(currentViewController!.view.bounds));
+            attachmentBehaviour = UIAttachmentBehavior(item: currentViewController!.view, offsetFromCenter: centerOffset, attachedToAnchor: location)
             animator.addBehavior(attachmentBehaviour)
         } else if (panRecognizer.state == .Changed) {
             attachmentBehaviour!.anchorPoint = location
@@ -127,13 +131,49 @@ class ModalNotificationViewController: UIViewController {
             
             let shouldDismiss = abs(velocity.x / velocityAdjust) > minimumVelocityRequiredForPush || abs(velocity.y / velocityAdjust) > minimumVelocityRequiredForPush
             if shouldDismiss {
+                let offsetFromCenter = UIOffsetMake(boxLocation.x - CGRectGetMidX(currentViewController!.view.bounds), boxLocation.y - CGRectGetMidY(currentViewController!.view.bounds))
+                let radius = sqrtf(powf(offsetFromCenter.horizontal, 2.0) + powf(offsetFromCenter.vertical, 2.0))
+                let pushVelocity = sqrtf(powf(velocity.x, 2.0) + powf(velocity.y, 2.0))
                 
+                // calculate angles needed for angular velocity formula
+                let velocityAngle = atan2f(velocity.y, velocity.x)
+                var locationAngle = atan2f(offsetFromCenter.vertical, offsetFromCenter.horizontal)
+                if (locationAngle > 0) {
+                    locationAngle -= Float(M_PI * 2.0)
+                }
+                
+                // angle (θ) is the angle between the push vector (V) and vector component parallel to radius, so it should always be positive
+                let angle = fabsf(fabsf(velocityAngle) - fabsf(locationAngle))
+                // angular velocity formula: w = (abs(V) * sin(θ)) / abs(r)
+                var angularVelocity = fabsf((fabsf(pushVelocity) * sinf(angle)) / fabsf(radius))
+                
+                // rotation direction is dependent upon which corner was pushed relative to the center of the view
+                // when velocity.y is positive, pushes to the right of center rotate clockwise, left is counterclockwise
+                var direction = (location.x < view.center.x) ? -1.0 : 1.0;
+                // when y component of velocity is negative, reverse direction
+                if (velocity.y < 0) { direction *= -1; }
+                
+                // amount of angular velocity should be relative to how close to the edge of the view the force originated
+                // angular velocity is reduced the closer to the center the force is applied
+                // for angular velocity: positive = clockwise, negative = counterclockwise
+                let xRatioFromCenter = fabsf(offsetFromCenter.horizontal) / (CGRectGetWidth(currentViewController!.view.frame) / 2.0)
+                let yRatioFromCetner = fabsf(offsetFromCenter.vertical) / (CGRectGetHeight(currentViewController!.view.frame) / 2.0)
+                
+                // apply device scale to angular velocity
+                angularVelocity *= Float(deviceAngularScale)
+                // adjust angular velocity based on distance from center, force applied farther towards the edges gets more spin
+                angularVelocity *= (xRatioFromCenter + yRatioFromCetner) / 2.0
+                
+                itemBehaviour!.addAngularVelocity(Float(angularVelocity * direction), forItem: currentViewController!.view)
+                animator.addBehavior(pushBehaviour)
+                pushBehaviour!.pushDirection = CGVectorMake(Float(velocity.x / velocityAdjust), Float(velocity.y / velocityAdjust))
+                pushBehaviour!.active = true
+                
+                // TODO: Move to next view controller
             } else {
                 animator.addBehavior(snapBehaviour)
                 animator.addBehavior(pushBehaviour)
             }
-            animator.addBehavior(snapBehaviour)
-            animator.addBehavior(pushBehaviour)
         }
     }
 }
